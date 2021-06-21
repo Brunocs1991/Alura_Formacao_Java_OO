7ff0160433ab9ca359e32921a33fcaef399e6cb5
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-06-21 20:07:28 -0300

    O contrato do método equals

--- Curso-Java-parte-8-Java-Collections/gerenciador-de-cursos/src/br/com/alura/classes/Aluno.java
+++ Curso-Java-parte-8-Java-Collections/gerenciador-de-cursos/src/br/com/alura/classes/Aluno.java
@@ -27,15 +27,43 @@ public class Aluno {
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-
-		Aluno outro = (Aluno) obj;
-		return this.nome.equals(outro.nome);
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + numeroMatriculoa;
+		return result;
 	}
 
 	@Override
-	public int hashCode() {
-		return this.nome.hashCode();
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (numeroMatriculoa != other.numeroMatriculoa)
+			return false;
+		return true;
 	}
+
+//	@Override
+//	public boolean equals(Object obj) {
+//
+//		Aluno outro = (Aluno) obj;
+//		return this.nome.equals(outro.nome);
+//	}
+//
+//	@Override
+//	public int hashCode() {
+//		return this.nome.hashCode();
+//	}
 
 }
