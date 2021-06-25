e443bb8e2ec6fbb4d1331b55683e190355e327cc
Brunocs1991 <brunocosta.1991@hotmail.com>
2021-06-24 21:50:32 -0300

    Testando um método que lança exception

--- 9-Java-Testes/java-testes-unitarios-tdd/src/main/java/br/com/alura/tdd/service/BonusService.java
+++ 9-Java-Testes/java-testes-unitarios-tdd/src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -10,7 +10,7 @@ public class BonusService {
 	public BigDecimal calcularBonus(Funcionario funcionario) {
 		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
 		if (valor.compareTo(new BigDecimal("1000")) > 0) {
-			valor = BigDecimal.ZERO;
+			throw new IllegalArgumentException("Funcionario com salario Maior que R$ 10.000,00 não pode receber bonus!");
 		}
 		return valor.setScale(2, RoundingMode.HALF_UP);
 	}
